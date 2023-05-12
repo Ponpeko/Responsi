2a8639791d59fe2b1e6d672f1ed1f6b667a95654
Ponpeko <128123487+Ponpeko@users.noreply.github.com>
2023-05-12 16:25:39 +0700

    Add files via upload

---
+++ Karyawan.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Responsi1
+{
+    public class Karyawan
+    {
+        public string Nama { get; set; }
+        public string NIK { get; set;}
+        public int GajiBulanan { get; set; }
+
+        public Karyawan (string nik, string nama, int gajiBulanan)
+        {
+            Nama = nama;
+            NIK = nik;
+            GajiBulanan = gajiBulanan;
+
+            if(gajiBulanan < 0)
+            {
+                gajiBulanan = 0;
+            }
+        }
+
+        public void naikGaji()
+        {
+            GajiBulanan = GajiBulanan + (GajiBulanan * 10 / 100);
+        }
+
+        public void printData(int no)
+        {
+            Console.WriteLine(no + ". {0} \t{1} \t{2}", NIK, Nama, GajiBulanan);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Responsi1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Karyawan karyawan1 = new Karyawan("12345", "Sigit Gutomo", 5000000);
+            Karyawan karyawan2 = new Karyawan("44321", "Abdul Azis", 4000000);
+
+            Console.WriteLine("No NIK \t\t Nama \t\tGaji Bulanan");
+            Console.WriteLine("----------------------------------------");
+
+            karyawan1.printData(1);
+            karyawan2.printData(2);
+
+            Console.WriteLine("\n\nAsyiiiik Kenaikan Gaji 10% !!!\n");
+
+            Console.WriteLine("No NIK \t\t Nama \t\tGaji Bulanan");
+            Console.WriteLine("----------------------------------------");
+
+            karyawan1.naikGaji();
+            karyawan2.naikGaji();
+
+            karyawan1.printData(1);
+            karyawan2.printData(2);
+        }   
+    }
+}
